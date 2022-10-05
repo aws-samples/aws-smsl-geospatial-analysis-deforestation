98ae1e972b0735d6eae7c5381dbf19d9dae0d424
Parth Patel <parth2651@gmail.com>
2022-10-05 10:56:22 -0700

    updated code to remove use of cred

--- sus201/lambda/asdi_retrieval/lambda_function.py
+++ sus201/lambda/asdi_retrieval/lambda_function.py
@@ -62,9 +62,6 @@ def lambda_handler(event, context):
     '''
     config.instance_id = os.environ['instance_id']
 
-    config.aws_access_key_id = os.environ['aws_access_key_id']
-    config.aws_secret_access_key = os.environ['aws_secret_access_key']
-
     if config.instance_id == '':
         print("Warning! To use WFS functionality, please configure the `instance_id`.")
     
@@ -83,51 +80,31 @@ def lambda_handler(event, context):
     #bands = ['B04.jp2','B08.jp2']
     totalCountToReturn = 4
     tiles = []
-    s3_urls = []
-    filtered_urls = []
+    #s3_urls = []
+    #filtered_urls = []
 
+    uniqueKeys = []
     for tile_info in wfs_iterator:
-        tiles.append(tile_info['properties']['path'])
-
-    totalUrlCount = len(tiles)
-    if(totalUrlCount < totalCountToReturn):
-        filtered_urls = s3_urls
-    else:
-        breakpoint = int (totalUrlCount/totalCountToReturn)
-        for count in range(0,totalUrlCount,breakpoint):
-            filtered_urls.append(tiles[count])
-            #for band in bands:
-                #s3_urls.append(tile_info['properties']['path'] + '/' + band)
-                #filtered_urls.append(tiles[count]+ '/' + band)
+        path = tile_info['properties']['path']
+        if(path.__contains__('tiles/10/S/FJ')):
+            pathEntries = path.split('/')
+            key = pathEntries[len(pathEntries)-4]+'-' + pathEntries[len(pathEntries)-3]
+            if(key not in uniqueKeys):
+                uniqueKeys.append(key)
+                tiles.append(tile_info['properties']['path'])
 
     sqs = boto3.client('sqs')
-    s3Folder = os.environ['s3Bucket'] + coordA + '-' + coordB + '-' + coordC + '-' + coordD + '-' + startDate + '-' + endDate
+    s3Folder = os.environ['s3Bucket'] + '-' + coordA + '-' + coordB + '-' + coordC + '-' + coordD 
     message = {}
     
-    for url in filtered_urls:
+    for url in tiles:
         message["s3Location"] = s3Folder
         message["asdi_url"] = url
         sqs.send_message(QueueUrl=os.environ['sqs_url'], MessageBody=json.dumps(message))
         print("sent message to sqs" + json.dumps(message) )
-
-    '''
-    wr.s3.copy_objects(
-        filtered_urls,
-        source_path="s3://sentinel-s2-l1c/",
-        target_path= "s3://reinvent-susty/source" + '-' + coordA + '-' + coordB + '-' + coordC + '-' + coordD,
-        use_threads= True,
-        #boto3_session = boto3session,
-        #replace_filenames={"tiles/10/T/FK/2018/11/1/0/B04.jp2": "11/1/0/B04.jp2"},
-        s3_additional_kwargs={
-            "RequestPayer": "requester"
-        }
-    )
-    '''        
+       
 
     print("End date and time: " + str(datetime.datetime.now()))
-    print("Filtered URLs")
-    print(filtered_urls)
-
     return {
         'statusCode': 200
     }
