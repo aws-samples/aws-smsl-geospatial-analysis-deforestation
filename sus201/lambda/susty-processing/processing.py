2525985cc45ff1c00719d8374282efd81ce21b08
Parth Patel <parth2651@gmail.com>
2022-10-16 22:07:20 -0700

    updated lambda

--- sus201/lambda/susty-processing/processing.py
+++ sus201/lambda/susty-processing/processing.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import json
 import boto3
 import io
+import numpy as np
+
 
 def lambda_handler(event, context):
     print('Event - ' + str(event))
@@ -54,13 +56,50 @@ def lambda_handler(event, context):
         print('complete reading')
         ndvi = (nirfloat-redfloat)/(nirfloat+redfloat)
 
+        # Create classes and apply to NDVI results
+        ndvi_class_bins = [-np.inf, 0, 0.1, 0.25, 0.4, np.inf]
+        ndvi_density_class = np.digitize(ndvi, ndvi_class_bins)
+        
+        # Apply the nodata mask to the newly classified NDVI data
+        ndvi_density_class = np.ma.masked_where(
+            np.ma.getmask(ndvi), ndvi_density_class
+        )
+        np.unique(ndvi_density_class)
+        
+        from matplotlib.colors import ListedColormap
+        # Define color map
+        nbr_colors = ["khaki", "y", "yellowgreen", "g", "darkgreen"]
+        nbr_cmap = ListedColormap(nbr_colors)
+        
+        # Define class names
+        ndvi_cat_names = [
+            "Dead forest",
+            "Scrub",
+            "Open Forest",
+            "Moderately Dense Forest",
+            "Very Dense Forest",
+        ]
+        # Get list of classes
+        classes = np.unique(ndvi_density_class)
+        classes = classes.tolist()
+        # The mask returns a value of none in the classes. remove that
+        classes = classes[0:5]
+
         print('before plt.figure')
-        fig = plt.figure(figsize=(10,10),num=1, clear=True)
-        ax = ax = fig.add_subplot(111)
-        cbar_plot = ax.imshow(ndvi.squeeze(), cmap=plt.cm.RdYlGn , vmin=-1, vmax=1)
-        ax.axis('off')
+        # fig = plt.figure(figsize=(20,20),num=1, clear=True)
+        # ax = ax = fig.add_subplot(111)
+        # cbar_plot = ax.imshow(ndvi.squeeze(), cmap=plt.cm.RdYlGn , vmin=-1, vmax=1)
+        # ax.axis('off')
+        # img_data = io.BytesIO()
+        # fig.savefig(img_data, dpi=50, bbox_inches='tight', pad_inches=0, format='png')
+        # img_data.seek(0)
+        
+        
+        fig, (ax1) = plt.subplots(1, figsize=(12, 12),num=1, clear=True)
+        im1 = ax1.imshow(np.squeeze(ndvi_density_class), cmap=nbr_cmap)
+        ax1.axis('off')
         img_data = io.BytesIO()
-        fig.savefig(img_data, dpi=50, bbox_inches='tight', pad_inches=0, format='png')
+        fig.savefig(img_data, dpi=100, bbox_inches='tight', pad_inches=0, format='png')
         img_data.seek(0)
         print('before plt.figure')
         
